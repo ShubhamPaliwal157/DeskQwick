c22ff4fb9b400847d8906c97211132d80ff7eb2d
ShubhamPaliwal157 <shubhampaliwal157@gmail.com>
2025-08-02 15:18:43 +0530

    Login page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Register Page</h2>
+    </div>
+  );
+};
+
+export default Register;
 
